5a4c9670f823c34573c1f72b88eb1ee9fdefd7f3
Saitama12-afro1 <saitama20012@gmail.com>
2023-04-07 17:05:10 +0300

    add async controller

---
+++ DjangoParse/app/controller.py
@@ -0,0 +1,26 @@
+import asyncio
+from asgiref.sync import sync_to_async
+
+from django.db import transaction
+from django.db.utils import IntegrityError
+
+from .models import Detail, Purchase
+
+class Controller:
+    @sync_to_async
+    def create(self, number, start_price):
+        print(number, start_price)
+        try:
+            with transaction.atomic():
+                purchase_obj = Purchase.objects.create(purchase_number = number, start_price =start_price)
+                Detail.objects.create(purchase = purchase_obj)
+        except IntegrityError as e:
+            print(e)
+            with transaction.atomic():
+                purchase_obj = Purchase.objects.get(purchase_number = number)
+                if purchase_obj.start_price != start_price:
+                    purchase_obj.start_price = start_price
+                    Detail.objects.get_or_create(purchase_id = number)
+                
+        return 0 
+    
